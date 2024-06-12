c66320ad9ff16505b7f8af1dfd547a20a3402e72
srikanth019 <srikanth.golla@brainvire.com>
2024-06-12 19:27:02 +0530

    Small changes related to Admin orders

--- src/features/admin/components/AdminOrders.js
+++ src/features/admin/components/AdminOrders.js
@@ -8,7 +8,6 @@ import {
     updateOrderAsync,
 } from '../../orders/OrderSlice';
 import {
-    PencilIcon,
     EyeIcon,
     ArrowUpIcon,
     ArrowDownIcon,
@@ -19,13 +18,16 @@ function AdminOrders () {
     const [page, setPage] = useState(1);
     const dispatch = useDispatch();
     const orders = useSelector(selectOrders);
-    console.log(/orders/, orders);
     const totalOrders = useSelector(selectTotalOrders);
     const [editableOrderId, setEditableOrderId] = useState(-1);
     const [sort, setSort] = useState({});
 
     const handleEdit = (order) => {
-        setEditableOrderId(order.id);
+        if (order.id === editableOrderId) {
+            setEditableOrderId(-1);
+        } else {
+            setEditableOrderId(order.id);
+        }
     };
     const handleShow = () => {
         console.log('handleShow');
@@ -185,10 +187,11 @@ function AdminOrders () {
                                                     ></EyeIcon>
                                                 </div>
                                                 <div className="w-6 mr-2 transform hover:text-purple-500 hover:scale-120">
-                                                    <PencilIcon
-                                                        className="w-8 h-8"
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7"
                                                         onClick={(e) => handleEdit(order)}
-                                                    ></PencilIcon>
+                                                    >
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                    </svg>
                                                 </div>
                                             </div>
                                         </td>
